be0c9e5104f883792e1ca53cc770cb97182c2a51
thedannysoto <87dsoto@gmail.com>
2020-04-30 08:18:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function mapToNegativize(arr) {
+    return arr.map(x => x * -1);
+}
+
+function mapToNoChange(arr) {
+    return arr.map(x => x);
+}
+
+function mapToDouble(arr) {
+    return arr.map(x => x * 2);
+}
+
+function mapToSquare(arr) {
+    return arr.map(x => x ** 2);
+}
+
+function reduceToTotal(arr, start=0) {
+    return arr.reduce((total, x) => total + x, start);
+}
+
+function reduceToAllTrue(arr, start=true) {
+    return arr.reduce((x, y) => {
+        if (!y) {
+            x = false;
+        }
+        return x;
+    }, start);
+}
+
+function reduceToAnyTrue(arr, start=false) {
+    return arr.reduce((x, y) => {
+        if (y) {
+            x = true;
+        }
+        return x;
+    }, start);
+}
